ad6c5422e2fb9650d1d2c38b829a9bc6a0f6542d
sudorook <daemon@nullcodon.com>
2019-03-22 13:00:54 -0500

    refactor gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,39 +1,38 @@
 /*
  * Gulpfile for building and updating the Capsule Hugo theme.
- */ 
+ */
 
 // Load npm packages
-const gulp   = require('gulp'),
-      concat = require('gulp-concat'),
-      cssmin = require('gulp-cssmin'),
-      print  = require('gulp-print').default,
-      rename = require('gulp-rename'),
-      sass   = require('gulp-sass'),
-      util   = require('gulp-util')
+const gulp = require('gulp');
+const cssmin = require('gulp-cssmin');
+const print = require('gulp-print').default;
+const rename = require('gulp-rename');
+const sass = require('gulp-sass');
+const util = require('gulp-util');
 
 
 /*
- * Set global variables: 
+ * Set global variables:
  *  - src: specifies source directories for the downloaded npm modules
  *  - build: specifies paths for directory for customizations of packages
- *      defaults 
+ *      defaults
  *  - dest: destination for processed dependencies ('static' by default in
- *      Hugo) 
+ *      Hugo)
  */
 
-// Specify where dependencies are stored. 
+// Specify where dependencies are stored.
 const src = {
   bulma: {
-    sass: './node_modules/bulma/sass/'
+    sass: './node_modules/bulma/sass/',
   },
   fontawesome: {
     fonts: './node_modules/font-awesome/fonts/',
     css: './node_modules/font-awesome/css/',
-    sass: './node_modules/font-awesome/scss/'
-  }
-}
+    sass: './node_modules/font-awesome/scss/',
+  },
+};
 
-// Specify build directory for sass, js, amd images. 
+// Specify build directory for sass, js, amd images.
 const build = {
   bulma: {
     sass: './build/sass/bulma/',
@@ -42,15 +41,15 @@ const build = {
     sass: './build/sass/fontawesome/',
   },
   sass: './build/sass/',
-  fonts:'./build/fonts/'
-}
+  fonts: './build/fonts/',
+};
 
-// Specify directory for processed dependencies. 
+// Specify directory for processed dependencies.
 const dest = {
   css: './static/css/',
   js: './static/js/',
-  fonts:'./static/fonts/'
-}
+  fonts: './static/fonts/',
+};
 
 
 /*
@@ -63,20 +62,22 @@ const dest = {
 // Compile css from sass files
 gulp.task('sass', function() {
   return gulp.src([build.sass + 'capsule.sass'])
-    .pipe(sass())
-    .pipe(print())
-    .pipe(cssmin())
-    .pipe(rename(function (name) {
-      name.extname = '.min.css';
-    }))
-    .pipe(gulp.dest(dest.css));
+      .pipe(sass())
+      .pipe(print())
+      .pipe(cssmin())
+      .pipe(rename(function(name) {
+        name.extname = '.min.css';
+      }))
+      .pipe(gulp.dest(dest.css));
 });
 
 // Copy fonts from node_modules
 gulp.task('fonts', function() {
   return gulp.src(src.fontawesome.fonts + '**/*')
-    .pipe(gulp.dest(dest.fonts))
-    .on('end', function() { util.log("Copied fonts."); });
+      .pipe(gulp.dest(dest.fonts))
+      .on('end', function() {
+        util.log('Copied fonts.');
+      });
 });
 
 // Rebuild css and fonts
